55d466453d8cb6d54b3031e29286da0522e21b29
jobskid <blueblueblueyk@gmail.com>
2021-07-08 08:40:51 +0900

    スクレイピング仮実装

--- StockApp/StockApp/MainWindowViewModel.cs
+++ StockApp/StockApp/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
-﻿using Prism.Mvvm;
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,16 +12,28 @@ namespace StockApp
 {
     internal class MainWindowViewModel : BindableBase
     {
-        private string stock;
-        public string Stock
+        private string stockValue;
+        public string StockValue
         {
-            get { return stock; }
-            set { SetProperty(ref stock, value); }
+            get { return stockValue; }
+            set { SetProperty(ref stockValue, value); }
         }
 
         public MainWindowViewModel()
         {
-            Stock = "a";
+            // 株価を取得したいサイトのURL
+            var code = "2317.T";
+            var urlstring = $"http://stocks.finance.yahoo.co.jp/stocks/detail/?code={code}";
+            // ページの HTML を取得
+            var client = new HttpClient();
+            var res = await client.GetStringAsync(urlstring);
+            // HTML をパース
+            var parser = new HtmlParser();
+            var doc = await parser.ParseDocumentAsync(res);
+            // クエリーセレクタを指定し株価部分を取得する
+            var nodes = doc.QuerySelectorAll("#main td[class=stoksPrice]");
+            
+            stockValue = nodes.ToString();
         }
     }
 }
